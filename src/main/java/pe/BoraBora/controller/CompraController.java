faaa2b4d4e8c768786f65f955772b9779adea38d
CarlosAcosta4 <carlosacosta4820@gmail.com>
2023-12-03 17:03:13 -0500

    Agregado el controlador para la busquedad de la compra de un usuario por su ID

--- src/main/java/pe/BoraBora/controller/CompraController.java
+++ src/main/java/pe/BoraBora/controller/CompraController.java
@@ -1,6 +1,5 @@
 package pe.BoraBora.controller;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,11 +8,9 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +19,6 @@ import pe.BoraBora.entity.Compra;
 import pe.BoraBora.entity.User;
 import pe.BoraBora.repository.UserRepository;
 import pe.BoraBora.response.ApiResponse;
-import pe.BoraBora.response.CompraProductoResponse;
 import pe.BoraBora.response.CompraResponse;
 import pe.BoraBora.service.CompraService;
 
@@ -36,25 +32,8 @@ public class CompraController {
     
     @Autowired
     private UserRepository userRepository;
-
-    //--REGISTRAR UNA COMPRA
-    @PostMapping("/register")
-    public ResponseEntity<ApiResponse> insert(@RequestBody Compra compra, HttpSession session) {
-        try {
-            User user = (User) session.getAttribute("user");
-            if (user != null && userRepository.existsById(user.getId())) {
-                compra.setUser(user);
-                compraService.insert(compra);
-                return new ResponseEntity<>(new ApiResponse("Compra insertada con éxito", HttpStatus.CREATED), HttpStatus.CREATED);
-            } else {
-                return new ResponseEntity<>(new ApiResponse("El usuario no existe o no ha iniciado sesión", HttpStatus.BAD_REQUEST), HttpStatus.BAD_REQUEST);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity<>(new ApiResponse("Error al insertar la compra. Detalles: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
     
-    //--BUSQUEDAD DE LA COMPRA DE UN USUARIO POR SU ID
+  //--BUSQUEDAD DE LA COMPRA DE UN USUARIO POR SU ID
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<CompraResponse>> findByUserId(@PathVariable Integer userId) {
         List<Compra> compras = compraService.findComprasByUserId(userId);
@@ -76,11 +55,28 @@ public class CompraController {
                 })
                 .collect(Collectors.toList());
             return new ResponseEntity<>(compraResponses, HttpStatus.OK);
-        } else {
+        } else {	
             CompraResponse compraResponse = new CompraResponse();
             compraResponse.setMessage("No se encontraron compras para el usuario");
             compraResponse.setStatus(HttpStatus.NOT_FOUND);
             return new ResponseEntity<>(List.of(compraResponse), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //--REGISTRAR UNA COMPRA
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse> insert(@RequestBody Compra compra, HttpSession session) {
+        try {
+            User user = (User) session.getAttribute("user");
+            if (user != null && userRepository.existsById(user.getId())) {
+                compra.setUser(user);
+                compraService.insert(compra);
+                return new ResponseEntity<>(new ApiResponse("Compra insertada con éxito", HttpStatus.CREATED), HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity<>(new ApiResponse("El usuario no existe o no ha iniciado sesión", HttpStatus.BAD_REQUEST), HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ApiResponse("Error al insertar la compra. Detalles: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
